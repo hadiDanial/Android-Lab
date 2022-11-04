9ff414d91232f86c1ad965cf3f635149939956d7
hadiDanial <hadiDanial@users.noreply.github.com>
2022-11-04 16:28:43 +0200

    Finished lab 1

--- Ex1/app/src/main/java/com/example/ex1/MainActivity.java
+++ Ex1/app/src/main/java/com/example/ex1/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.layout2);
+//        setContentView(R.layout.layout3);
+//        setContentView(R.layout.layout4);
     }
 }
