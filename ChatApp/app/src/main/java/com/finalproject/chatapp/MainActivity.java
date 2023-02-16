ac2b319fd61245a684e8127563703a15d2866abb
hadiDanial <hadiDanial@users.noreply.github.com>
2023-02-16 15:23:46 +0200

    Final Project

---
+++ ChatApp/app/src/main/java/com/finalproject/chatapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.finalproject.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.finalproject.chatapp.fragments.Register;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser == null) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            Fragment f = new Register();
+            ft.add(R.id.mainContainer, f).addToBackStack("registerFragment").commit();
+        }
+        else {
+
+        }
+    }
+}
