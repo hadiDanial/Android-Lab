1483c1d01d2c875d8922a2e1869d7777a335d8cb
hadiDanial <hadiDanial@users.noreply.github.com>
2023-02-16 18:37:32 +0200

    Added login using Firebase AuthUI

--- ChatApp/app/src/main/java/com/finalproject/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/com/finalproject/chatapp/MainActivity.java
@@ -1,29 +1,92 @@
 package com.finalproject.chatapp;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.finalproject.chatapp.fragments.LoginFragment;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", user.getDisplayName());
+        map.put("email", user.getEmail());
+        map.put("phone", user.getPhoneNumber());
+        databaseReference.setValue(map);
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child("456");
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("K1", "Data");
+//        map.put("Test", "Hello");
+//        databaseReference.setValue(map);
+
+
+
+
         if(firebaseUser == null) {
-            FragmentManager supportFragmentManager = getSupportFragmentManager();
-            FragmentTransaction ft = supportFragmentManager.beginTransaction();
-            Fragment f = new LoginFragment(supportFragmentManager);
-            ft.add(R.id.mainContainer, f).addToBackStack("login").commit();
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.PhoneBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+            signInLauncher.launch(signInIntent);
+//            FragmentManager supportFragmentManager = getSupportFragmentManager();
+//            FragmentTransaction ft = supportFragmentManager.beginTransaction();
+//            Fragment f = new LoginFragment(supportFragmentManager);
+//            ft.add(R.id.mainContainer, f).addToBackStack("login").commit();
         }
         else {
 // to do
