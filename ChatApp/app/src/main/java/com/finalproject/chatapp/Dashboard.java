42ede36899ad9b9ebd67d29084e43808809b41de
hadiDanial <hadiDanial@users.noreply.github.com>
2023-02-17 03:01:28 +0200

    Options menu in dashboard (logout, exit)

--- ChatApp/app/src/main/java/com/finalproject/chatapp/Dashboard.java
+++ ChatApp/app/src/main/java/com/finalproject/chatapp/Dashboard.java
@@ -1,14 +1,47 @@
 package com.finalproject.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
-
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.Logout:
+            {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            case R.id.Exit:
+            {
+                finish();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
