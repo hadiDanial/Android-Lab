d5f256d0a9a71fcf3e093b0b13daab6ddf606c8e
hadiDanial <hadiDanial@users.noreply.github.com>
2023-02-19 17:53:40 +0200

    Implemented profile page

--- ChatApp/app/src/main/java/com/finalproject/chatapp/fragments/Profile.java
+++ ChatApp/app/src/main/java/com/finalproject/chatapp/fragments/Profile.java
@@ -1,14 +1,33 @@
 package com.finalproject.chatapp.fragments;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.finalproject.chatapp.ChatActivity;
 import com.finalproject.chatapp.R;
+import com.finalproject.chatapp.Utility;
+import com.finalproject.chatapp.controllers.MessageController;
+import com.finalproject.chatapp.controllers.UserController;
+import com.finalproject.chatapp.models.Chat;
+import com.finalproject.chatapp.models.User;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,34 +35,21 @@ import com.finalproject.chatapp.R;
  * create an instance of this fragment.
  */
 public class Profile extends Fragment {
+    private final User user;
+    private View clearConversation, sendMessage, editButton, editActionViews, cancelChanges, saveChanges;
+    private EditText displayNameText, firstNameText, lastNameText, statusText, phoneNumberText;
+    private TextView onlineStatus;
+    private View[] viewsForOtherUsers, viewsForLoggedInUser;
+    private boolean isLoggedInUserProfile;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Profile() {
-        // Required empty public constructor
+    public Profile(User user) {
+        this.user = user;
+        Log.i("", "Profile: " + user);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
+    public static Profile newInstance(User user) {
+        Profile fragment = new Profile(user);
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +58,129 @@ public class Profile extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        String loggedInID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        isLoggedInUserProfile = loggedInID.equals(user.getuID());
+        displayNameText  = view.findViewById(R.id.profileName);
+        firstNameText = view.findViewById(R.id.profileFirstNameEdit);
+        lastNameText = view.findViewById(R.id.profileLastNameEdit);
+        statusText = view.findViewById(R.id.profileStatusEdit);
+        phoneNumberText = view.findViewById(R.id.profilePhoneNumber);
+        onlineStatus = view.findViewById(R.id.profileOnlineStatus);
+
+        sendMessage = view.findViewById(R.id.sendMessage);
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isLoggedInUserProfile) return;
+                Intent intent = new Intent(getContext(), ChatActivity.class);
+                intent.putExtra("userID", loggedInID);
+                intent.putExtra("otherUserID", user.getuID());
+                getContext().startActivity(intent);
+            }
+        });
+        editButton = view.findViewById(R.id.editProfileDetails);
+        editActionViews = view.findViewById(R.id.editActionsView);
+        editActionViews.setVisibility(View.GONE);
+        cancelChanges = view.findViewById(R.id.cancelChanges);
+        saveChanges = view.findViewById(R.id.saveChanges);
+
+        saveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setPhoneNumber(phoneNumberText.getText().toString().trim());
+                user.setFirstName(firstNameText.getText().toString().trim());
+                user.setLastName(lastNameText.getText().toString().trim());
+                user.setDisplayName(displayNameText.getText().toString().trim());
+                UserController.updateUser(user);
+                setProfileEditable(false);
+                ((InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0); // Hide keyboard
+            }
+        });
+        cancelChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setProfileEditable(false);
+                setProfileDetails();
+            }
+        });
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setProfileEditable(true);
+            }
+        });
+
+        clearConversation = view.findViewById(R.id.cardClearConversation);
+        clearConversation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogInterface.OnClickListener positiveButtonAction = (dialog, id) -> MessageController.
+                        clearChat(Chat.getChatID(FirebaseAuth.getInstance().getCurrentUser().getUid(), user.getuID()));
+
+                DialogInterface.OnClickListener negativeButtonAction = (dialog, id) -> dialog.cancel();
+                String alertMessage = "Are you sure you want to delete this conversation?";
+                String positiveButtonText = "Delete", negativeButtonText = "Cancel";
+                boolean isCancelable = true;
+
+                Utility.generateAlertDialog(positiveButtonAction, negativeButtonAction, alertMessage, positiveButtonText, negativeButtonText, isCancelable, getContext());
+            }
+        });
+
+        setVisibility();
+
+        setProfileDetails();
+    }
+
+    private void setProfileDetails() {
+        displayNameText.setText(user.getDisplayName());
+        lastNameText.setText(user.getLastName());
+        firstNameText.setText(user.getFirstName());
+        statusText.setText(user.getStatus());
+        phoneNumberText.setText(user.getPhoneNumber());
+    }
+
+    private void setProfileEditable(boolean enabled) {
+        int bgColor = enabled ? R.color.white : R.color.transparent, textColor = enabled ? R.color.black : R.color.white;
+        displayNameText.setEnabled(enabled);
+        firstNameText.setEnabled(enabled);
+        lastNameText.setEnabled(enabled);
+        statusText.setEnabled(enabled);
+        phoneNumberText.setEnabled(enabled);
+
+        if(enabled)
+            editActionViews.setVisibility(View.VISIBLE);
+        else
+            editActionViews.setVisibility(View.GONE);
+    }
+
+    private void setVisibility() {
+        if(isLoggedInUserProfile)
+        {
+            editButton.setVisibility(View.VISIBLE);
+            clearConversation.setVisibility(View.GONE);
+            sendMessage.setVisibility(View.GONE);
+            onlineStatus.setVisibility(View.GONE);
+        }
+        else
+        {
+            editButton.setVisibility(View.GONE);
+            clearConversation.setVisibility(View.VISIBLE);
+            sendMessage.setVisibility(View.VISIBLE);
+            onlineStatus.setText(user.isOnline() ? "Online" : "Offline");
+        }
     }
 }
