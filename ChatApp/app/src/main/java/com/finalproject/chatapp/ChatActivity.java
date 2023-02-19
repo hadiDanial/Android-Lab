350d2099afd3772649892aad8ea27d257f5ea801
hadiDanial <hadiDanial@users.noreply.github.com>
2023-02-19 02:43:24 +0200

    Update ChatActivity.java

--- ChatApp/app/src/main/java/com/finalproject/chatapp/ChatActivity.java
+++ ChatApp/app/src/main/java/com/finalproject/chatapp/ChatActivity.java
@@ -17,58 +17,68 @@ import com.finalproject.chatapp.adapters.MessageViewModel;
 import com.finalproject.chatapp.controllers.UserController;
 import com.finalproject.chatapp.models.Chat;
 import com.finalproject.chatapp.models.Message;
+import com.finalproject.chatapp.services.NetworkBroadcastReceiver;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity implements View.OnClickListener, MessageAdapter.IMessageClickListener {
     private String userID, otherUserID, chatID;
-    private FirebaseAuth firebaseAuth;
     private RecyclerView recyclerView;
     private ImageButton sendButton;
     private DatabaseReference messagesDatabase;
     private EditText messageText;
     private MessageAdapter messageAdapter;
+    private NetworkBroadcastReceiver networkBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        sendButton = findViewById(R.id.btnSend);
+        messageText = findViewById(R.id.msgText);
+
+        recyclerView = findViewById(R.id.recyclerViewMessage);
         Bundle bundle = getIntent().getExtras();
         userID = bundle.getString("userID");
         otherUserID = bundle.getString("otherUserID");
+
         Chat chat = new Chat(userID, otherUserID);
         chatID = chat.getChatID();
-        sendButton = findViewById(R.id.btnSend);
         sendButton.setOnClickListener(this);
-        messageText = findViewById(R.id.msgText);
-        firebaseAuth = FirebaseAuth.getInstance();
+
         MessageViewModel messageViewModel = new ViewModelProvider(this).get(MessageViewModel.class);
         messageViewModel.setChatID(chatID);
         messageAdapter = new MessageAdapter(this, messageViewModel, getSupportFragmentManager(), this);
-        recyclerView = findViewById(R.id.recyclerViewMessage);
+
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(messageAdapter);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        messagesDatabase = FirebaseDatabase.getInstance().getReference("Messages").child(chatID);
         recyclerView.scrollToPosition(messageAdapter.getItemCount() - 1);
 
+        messagesDatabase = FirebaseDatabase.getInstance().getReference("Messages").child(chatID);
+        networkBroadcastReceiver = new NetworkBroadcastReceiver(getSupportFragmentManager(), R.id.chatContainer, this, findViewById(R.id.writeMessageLayout));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         UserController.setOnlineStatus(true);
+        networkBroadcastReceiver.register();
     }
 
     @Override
     protected void onPause() {
         UserController.setOnlineStatus(false);
+        networkBroadcastReceiver.unregister();
         super.onPause();
     }
 
+    // Send a new message
     @Override
     public void onClick(View v) {
         String msg = messageText.getText().toString().trim();
@@ -81,16 +91,20 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /**
+     *  Delete message
+     * @param message Message to delete
+     */
     @Override
     public void DeleteMessage(Message message) {
-        // TODO: Open dialog asking if user is sure they want to delete the message
+        // Open dialog asking if user is sure they want to delete the message
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage("Are you sure you want to delete this message?");
         alertDialogBuilder.setCancelable(true);
+
         alertDialogBuilder.setPositiveButton("Delete", (dialog, id) -> messagesDatabase.child(message.getDate()).removeValue());
         alertDialogBuilder.setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
 
         alertDialogBuilder.create().show();
-
     }
 }
